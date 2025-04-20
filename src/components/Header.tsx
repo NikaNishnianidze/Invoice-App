250c9b969424b5e8ce69b2823332559e0ddd2c51
EMIS <EMIS@A4773-9.schools.emis.ge>
2025-04-20 19:26:27 +0400

    add Header layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,41 @@
+import { useState } from "react";
+import logo from "../../public/assets/logo.svg";
+import moonIcon from "../../public/assets/icon-moon.svg";
+import sunIcon from "../../public/assets/icon-sun.svg";
+import avatar from "../../public/assets/image-avatar.jpg";
+
 export default function Heeader() {
-  return <></>;
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+
+  const handleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+
+    if (!darkMode) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  };
+  return (
+    <div className="flex justify-center">
+      <header className="w-[375px] flex items-center justify-between">
+        <div className="logo">
+          <img src={logo} alt="logo icon" />
+        </div>
+        <div className="dark-avatar flex items-center gap-[24px] mr-[24px]">
+          <img
+            src={darkMode ? sunIcon : moonIcon}
+            alt="dark mode icons"
+            onClick={handleDarkMode}
+          />
+          <div className="divider w-[1px] h-[72px] bg-divider"></div>
+          <img
+            src={avatar}
+            alt="avatar image"
+            className="w-[32px] h-[32px] rounded-[50%]"
+          />
+        </div>
+      </header>
+    </div>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Navigate } from "react-router-dom";
 import "./index.css";
-import { createBrowserRouter } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Layout from "./Layout/Layout";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <Navigate to={"/invoices"} />,
+  },
+  {
+    path: "/invoices",
     element: <Layout />,
   },
 ]);
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode></StrictMode>
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 );
